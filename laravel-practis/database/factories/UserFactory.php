20ab025a84b2487657cbb8f3060f0380aee8ec2b
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2023-05-29 18:25:56 +0900

    UserSeeder　修正

--- laravel-practis/database/factories/UserFactory.php
+++ laravel-practis/database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -23,6 +24,7 @@ class UserFactory extends Factory
                 return Company::query()->inRandomOrder()->first()->id;
             },
             'name' => fake()->name(),
+            'role' => User::ROLE_USER,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -45,6 +47,7 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'name' => 'Kosuke Shibuya',
             'email' => 'admin@example.com',
+            'role' => User::ROLE_ADMIN,
         ]);
     }
 }
